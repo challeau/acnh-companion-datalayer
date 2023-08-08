2d177407d1f6607c1d03243ba6e07b09e0b26b1a
Charlotte Alleaume <charlottealleaume99@gmail.com>
2023-08-08 04:47:52 +0200

    m

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 // Link frontend
 app.use(cors({
-  origin: ['http://localhost:3000', process.env.FRONTEND_URL],
+  origin: [process.env.FRONTEND_URL],
   credentials: true
 }));
 
